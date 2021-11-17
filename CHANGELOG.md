aa608fd63d785d6cde16d448ef37814a2de12871
simphotonics <git@simphotonics.com>
2021-11-17 22:38:47 +0000

    Published version 0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,8 @@
 
+## Version 0.0.2
+
+Formatted benchmark output.
+
 ## Version 0.0.1
 
 Initial commit.
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="lockattrs",
-    version="0.0.1",
+    version="0.0.2",
     author="D Reschner",
     author_email="git@simphotonics.com",
     description="Decorator used to lock class attributes.",
