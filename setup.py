c26aae19e5878b4c7342c808e661e04f2162d118
simphotonics <git@simphotonics.com>
2021-12-01 17:52:51 +0000

    Incremented version no.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="lockattrs",
-    version="0.0.4",
+    version="0.0.5",
     author="D Reschner",
     author_email="git@simphotonics.com",
     description="Decorator used to lock class attributes.",
